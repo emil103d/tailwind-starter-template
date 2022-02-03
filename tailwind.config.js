4f489a8eac2c92927e07964061086a0ac590e22f
emilie carlsen <emil103d@stud.kea.dk>
2022-02-03 14:56:33 +0100

    Torsdag

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#362222",
+        accent: "#F76E11",
+        hover: "#FF9F45",
+      },
+      fontFamily: {
+        monoton: "'Monoton', system-ui, sans-serif",
+        sora: "'sora', system-ui, sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
